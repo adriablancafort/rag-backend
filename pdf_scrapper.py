7dc7db923d9b589b459d25ea5bafb2c2d5ec9e3d
Adrià Blancafort <76774853+adriablancafort@users.noreply.github.com>
2024-05-05 00:27:24 +0200

    Add files via upload

---
+++ pdf_scrapper.py
@@ -0,0 +1,16 @@
+import requests
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+
+def load_documents(pdf_folder_path: str):
+    document_loader = PyPDFDirectoryLoader(pdf_folder_path)
+    return document_loader.load()
+# Function to load PDF
+
+def text_from_pdfURL(URL: str):
+    response = requests.get(URL)
+    pdf_path = "data/Downloaded"
+    pdf = open(pdf_path + "/downloaded_data.pdf", "wb")
+    pdf.write(response.content)
+    pdf.close()
+    document = load_documents(pdf_path)
+    return document
